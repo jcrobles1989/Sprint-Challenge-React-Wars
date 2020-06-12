4eea7f88a1d2b408e502dda73636f48bc4d7f010
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-06-12 11:00:48 -0700

    finished

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Col, Card, CardBody, CardHeader, CardText } from 'reactstrap';
+import { Col, Card, CardImg, CardBody, CardHeader, CardText } from 'reactstrap';
 
 const CharacterCard = function(props) {
 
     return (
         <Col xs='12' sm='6' md='4' xl='3'>
             <Card className = 'card-container'>
+                <CardImg src={props.character.image}/>
                 <CardBody>
                     <CardHeader className = 'card-title'>{props.character.name}</CardHeader>
                     <CardText>URL: <a className = 'link' href={props.character.url}>{props.character.url}</a></CardText>
